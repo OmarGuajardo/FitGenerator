65497f1e95c6a4128995367a2345b4db0644af15
OmarGuajardo <omar.guajardog@outlook.com>
2020-07-13 10:32:26 -0700

    Register Activity Skeleton Finished

--- app/src/main/java/com/example/fitgenerator/RegisterActivity.java
+++ app/src/main/java/com/example/fitgenerator/RegisterActivity.java
@@ -6,28 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.example.fitgenerator.databinding.ActivityLoginBinding;
+import com.example.fitgenerator.databinding.ActivityRegisterBinding;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
-    ActivityLoginBinding binding;
+    ActivityRegisterBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
-        binding.btnLogIn.setOnClickListener(new View.OnClickListener() {
+        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
-        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
+        binding.btnReturn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
