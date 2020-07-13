01c07b6082c5701817e86a4405996244ee43fd44
OmarGuajardo <omar.guajardog@outlook.com>
2020-07-13 11:11:04 -0700

    MainActivity Slides Skeleton Finished

--- app/src/main/java/com/example/fitgenerator/MainActivity.java
+++ app/src/main/java/com/example/fitgenerator/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.fitgenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.example.fitgenerator.databinding.ActivityMainBinding;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+    TabLayout tabLayout;
+    TabItem tabCloset;
+    TabItem tabFits;
+    TabItem tabLaundry;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+
+        tabLayout = findViewById(R.id.tabBar);
+
+        tabLayout.setupWithViewPager(binding.viewPager);
+        final PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager()
+                ,0);
+
+        pagerAdapter.addFragment(new ClosetFragment(),"Closet");
+        pagerAdapter.addFragment(new FitsFragment(),"Fits");
+        pagerAdapter.addFragment(new LaundryFragment(),"Laundry");
+        binding.viewPager.setAdapter(pagerAdapter);
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/fitgenerator/PagerAdapter.java
@@ -0,0 +1,43 @@
+package com.example.fitgenerator;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    private int numOfTabs;
+    private List<Fragment> fragments = new ArrayList<>();
+    private List<String> fragmentTitle = new ArrayList<>();
+
+    public PagerAdapter(FragmentManager fm, int behavior){
+            super(fm,behavior);
+    }
+
+    public void addFragment(Fragment fragment,String title){
+        fragments.add(fragment);
+        fragmentTitle.add(title);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+            return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitle.get(position);
+    }
+}
