677cb9f9ad2520d177948a44029f89addffdc11f
OmarGuajardo <omar.guajardog@outlook.com>
2020-07-24 11:48:44 -0700

    Basic layout for ChooseFit

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/example/fitgenerator/databinding/ActivityChooseFitBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/example/fitgenerator/databinding/ActivityChooseFitBinding.java
@@ -4,20 +4,45 @@ package com.example.fitgenerator.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewbinding.ViewBinding;
 import com.example.fitgenerator.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class ActivityChooseFitBinding implements ViewBinding {
   @NonNull
   private final ConstraintLayout rootView;
 
-  private ActivityChooseFitBinding(@NonNull ConstraintLayout rootView) {
+  @NonNull
+  public final FloatingActionButton btnFAB;
+
+  @NonNull
+  public final ImageView ivBottom;
+
+  @NonNull
+  public final ImageView ivLayer;
+
+  @NonNull
+  public final ImageView ivShoes;
+
+  @NonNull
+  public final ImageView ivTop;
+
+  private ActivityChooseFitBinding(@NonNull ConstraintLayout rootView,
+      @NonNull FloatingActionButton btnFAB, @NonNull ImageView ivBottom, @NonNull ImageView ivLayer,
+      @NonNull ImageView ivShoes, @NonNull ImageView ivTop) {
     this.rootView = rootView;
+    this.btnFAB = btnFAB;
+    this.ivBottom = ivBottom;
+    this.ivLayer = ivLayer;
+    this.ivShoes = ivShoes;
+    this.ivTop = ivTop;
   }
 
   @Override
@@ -43,10 +68,44 @@ public final class ActivityChooseFitBinding implements ViewBinding {
 
   @NonNull
   public static ActivityChooseFitBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.btnFAB;
+      FloatingActionButton btnFAB = rootView.findViewById(id);
+      if (btnFAB == null) {
+        break missingId;
+      }
 
-    return new ActivityChooseFitBinding((ConstraintLayout) rootView);
+      id = R.id.ivBottom;
+      ImageView ivBottom = rootView.findViewById(id);
+      if (ivBottom == null) {
+        break missingId;
+      }
+
+      id = R.id.ivLayer;
+      ImageView ivLayer = rootView.findViewById(id);
+      if (ivLayer == null) {
+        break missingId;
+      }
+
+      id = R.id.ivShoes;
+      ImageView ivShoes = rootView.findViewById(id);
+      if (ivShoes == null) {
+        break missingId;
+      }
+
+      id = R.id.ivTop;
+      ImageView ivTop = rootView.findViewById(id);
+      if (ivTop == null) {
+        break missingId;
+      }
+
+      return new ActivityChooseFitBinding((ConstraintLayout) rootView, btnFAB, ivBottom, ivLayer,
+          ivShoes, ivTop);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
--- app/src/main/java/com/example/fitgenerator/activities/ChooseFit.java
+++ app/src/main/java/com/example/fitgenerator/activities/ChooseFit.java
@@ -6,8 +6,10 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.fitgenerator.R;
 import com.example.fitgenerator.databinding.ActivityChooseFitBinding;
 
@@ -29,6 +31,24 @@ public class ChooseFit extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         TextView tvToolBarTitle = toolbar.findViewById(R.id.tvToolBarTitle);
         tvToolBarTitle.setText("Choose Fit");
+
+        Glide.with(getApplicationContext())
+                .load("https://lp2.hm.com/hmgoepprod?set=quality[79],source[/c1/b1/c1b1bbc63a23984e66f3e32f1ce5b45ad8b0956f.jpg],origin[dam],category[men_tshirtstanks_shortsleeve],type[LOOKBOOK],res[m],hmver[1]&call=url[file:/product/main]")
+                .fitCenter()
+                .into(binding.ivTop);
+        Glide.with(getApplicationContext())
+                .load("https://lp2.hm.com/hmgoepprod?set=quality[79],source[/b9/fc/b9fc3df031aa5903d876dad04d3cbd462d43b726.jpg],origin[dam],category[men_trousers_chinos_skinny_all],type[LOOKBOOK],res[m],hmver[1]&call=url[file:/product/main]")
+                .fitCenter()
+                .into(binding.ivBottom);
+        Glide.with(getApplicationContext())
+                .load("https://lp2.hm.com/hmgoepprod?set=quality[79],source[/bb/2c/bb2cc7a85ae612d153a031e2657821cca6da474c.jpg],origin[dam],category[men_jacketscoats_bikerjackets],type[LOOKBOOK],res[s],hmver[1]&call=url[file:/product/main]")
+                .fitCenter()
+                .into(binding.ivLayer);
+        Glide.with(getApplicationContext())
+                .load("https://lp2.hm.com/hmgoepprod?set=quality[79],source[/9c/41/9c4105e213f927ca3430ab502e1de5edabeb9263.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[s],hmver[1]&call=url[file:/product/main]")
+                .fitCenter()
+                .into(binding.ivShoes);
+
     }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
